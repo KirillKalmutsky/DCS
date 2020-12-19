592514c616fd7a7564ed74090091363251579350
Кирилл Кальмуцкий <sky-reaper@ya.ru>
2020-12-19 12:13:45 +0700

    homework_01 added

--- Homework_01/app.py
+++ Homework_01/app.py
@@ -1,9 +1,14 @@
 import numpy as np
+import pandas as pd
 
 
 if __name__ == '__main__':
-    print('Print some numbers:')
+    print('\nPrint some numbers:')
     digits = input()
     digits = [int(num) for num in digits.split(' ')]
-    print('Median of numbers is:', np.median(digits))
-    print('Mean of numbers is:', np.mean(digits))
+    print('Median of numbers is:', round(np.median(digits)))
+    print('Mean of numbers is:', round(np.mean(digits)))
+    df = pd.DataFrame(np.random.randint(0,100,size=(10, 4)), columns=list('ABCD'))
+    print('\nAlso show that pandas works well:\n')
+    print(df)
+    
