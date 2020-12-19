184a0d6cc1867cdfd3852d36085bac95eeeb192a
Кирилл Кальмуцкий <sky-reaper@ya.ru>
2020-12-19 12:24:39 +0700

    readme.md

--- Homework_01/app.py
+++ Homework_01/app.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     digits = [int(num) for num in digits.split(' ')]
     print('Median of numbers is:', round(np.median(digits)))
     print('Mean of numbers is:', round(np.mean(digits)))
-    df = pd.DataFrame(np.random.randint(0,100,size=(10, 4)), columns=list('ABCD'))
+    df = pd.DataFrame(np.random.randint(1,100,size=(10, 4)), columns=list('ABCD'))
     print('\nAlso show that pandas works well:\n')
     print(df)
     
